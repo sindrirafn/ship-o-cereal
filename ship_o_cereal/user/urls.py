eaa506f7e7f116503c40b29067a76d88244657b5
Sindri Rafn <sindrig15@ru.is>
2021-05-05 18:16:42 +0000

    Miklar breytingar í base.html og tok thvi midur ut bitchboi banter til ad gera siduna skiljanlegri

--- ship_o_cereal/user/urls.py
+++ ship_o_cereal/user/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     # hhtp://localhost:8000/users
-    path('', views.index, name="index"),
+    path('', views.index, name="user-index"),
 ]
