c74b28e8a0d45bf463d820639f0cca2b6c23a42e
jonevert <jonp16@ru.is>
2021-05-06 01:58:57 +0000

    Setti inn dummy morgunkorn og myndir og let thaer birtast undir cereals

--- ship_o_cereal/cereal/views.py
+++ ship_o_cereal/cereal/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 # Create your views here.
 
+cereals = [
+    {'name': 'Cocoa Puffs', 'price': 420},
+    {'name': 'Rice Krispies', 'price': 69},
+    {'name': 'Cheerios', 'price': 6969},
+
+]
+
 def index(request):
-    return render(request, 'cereal/index.html')
+    return render(request, 'cereal/index.html', context={'cereals':cereals})
