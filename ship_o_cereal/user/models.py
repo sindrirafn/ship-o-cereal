c28a33d28398a2727b2b96c341a3ce288dc0155b
jonevert <jonp16@ru.is>
2021-05-06 20:32:30 +0000

    initialized 'models' for users, cerea and merch. not complete

--- ship_o_cereal/user/models.py
+++ ship_o_cereal/user/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    firstName = models.CharField(max_length=255)
+    lastName = models.CharField(max_length=255)
+    userName = models.CharField(max_length=255)
+    cereal = models.ForeignKey(Cereal, on_delete=models.CASCADE)
+
+class UserImage(models.Model):
+    image = models.CharField(max_length=9999)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
