250e18c66b5ad0e784f5ed3554ad733b608d9e77
Steinar <steisaks@gmail.com>
2021-05-08 17:19:54 +0000

    Edit profile komið.

--- ship_o_cereal/user/views.py
+++ ship_o_cereal/user/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'user/index.html')
+
+def item(request):
+    return render(request, 'user/edit_profile.html')
