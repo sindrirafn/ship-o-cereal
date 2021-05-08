2c9838d967a36b57cc159f34f6dd16ee13271ae1
sindrirafn <sindrirafn12345@gmail.com>
2021-05-08 17:47:55 +0000

    login og register user síður komnar. checkið a /login og /register

--- ship_o_cereal/front_page/urls.py
+++ ship_o_cereal/front_page/urls.py
@@ -3,4 +3,6 @@ from . import views
 urlpatterns = [
     # http://localhost:8000/
     path('', views.index, name="frontpage-index"),
+    path('register', views.register, name="register-index"),
+    path('login', views.login, name="login-index"),
 ]
--- ship_o_cereal/front_page/views.py
+++ ship_o_cereal/front_page/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'front_page/index.html')
+
+def register(request):
+    return render(request, 'front_page/register.html')
+
+def login(request):
+    return render(request, 'front_page/login.html')
