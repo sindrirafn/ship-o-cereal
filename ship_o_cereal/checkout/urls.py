40a941fa3c34cf9e2e2252012ac81e0320e57561
sindrirafn <sindrirafn12345@gmail.com>
2021-05-09 17:03:03 +0000

    baetti contact info i checkout

--- ship_o_cereal/checkout/urls.py
+++ ship_o_cereal/checkout/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     # http://localhost:8000/payment
     path('', views.index, name="checkout-index"),
+    path('contactInfo', views.contact, name="checkout-contact"),
 ]
