caa1e89c26c6faf14d4b8dc5809b9ef4670045ea
sindrirafn <sindrirafn12345@gmail.com>
2021-05-11 16:13:30 +0000

    product details ordid fínt og myndastaerd í list fixed

--- ship_o_cereal/cereal/views.py
+++ ship_o_cereal/cereal/views.py
@@ -2,21 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from cereal.models import Cereal
 # Create your views here.
 
-# cereals = [
-#     {'name': 'Cocoa Puffs', 'price': 420},
-#     {'name': 'Rice Krispies', 'price': 69},
-#     {'name': 'Cheerios', 'price': 6969},
-#
-# ]
 
 def index(request):
     if 'orderBy' in request.GET:
         orderParameter = request.GET['orderBy']
         context = {'cereals': Cereal.objects.all().order_by(orderParameter)}
-
-    elif 'searchStr' in request.GET:
-        searchParameter = request.GET['searchStr']
-        context = {'cereals': Cereal.objects.filter(name__icontains=searchParameter).order_by('name')}
     else:
         context = {'cereals': Cereal.objects.all().order_by('name')}
 
