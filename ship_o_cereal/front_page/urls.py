177b7d6f345ba6fc7a7927c7265c6e66c90009e9
Steinar <steisaks@gmail.com>
2021-05-11 00:58:43 +0000

    log in, log out og register, eh smá js í cart.

--- ship_o_cereal/front_page/urls.py
+++ ship_o_cereal/front_page/urls.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from . import views
 urlpatterns = [
     # http://localhost:8000/
     path('', views.index, name="frontpage-index"),
     path('register', views.register, name="register-index"),
-    path('login', views.login, name="login-index"),
+    path('login', LoginView.as_view(template_name='front_page/login.html'), name='login-index'),
+    path('logout', LogoutView.as_view(next_page='login-index'), name='logout-index')
 ]
--- ship_o_cereal/front_page/views.py
+++ ship_o_cereal/front_page/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
@@ -6,7 +7,11 @@ def index(request):
     return render(request, 'front_page/index.html')
 
 def register(request):
-    return render(request, 'front_page/register.html')
-
-def login(request):
-    return render(request, 'front_page/login.html')
+    if request.method == 'POST':
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login-index')
+    return render(request, 'front_page/register.html', {
+        'form': UserCreationForm()
+    })
