617f3109afe3718afb29192551944ab9d0dcc8c6
sindrirafn <sindrirafn12345@gmail.com>
2021-05-12 14:19:55 +0000

    smá commit svo allt sé up to date

--- ship_o_cereal/cereal/urls.py
+++ ship_o_cereal/cereal/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     # http://localhost:8000/cereals
     path('', views.index, name="products-index"),
     # path('<int:id>', views.cereal_by_id, name="cereal-details"),
-    path('<str:name>', views.cereal_by_name, name="product-details"),
+    path('<str:name>', views.product_by_name, name="product-details"),
 ]
--- ship_o_cereal/cereal/views.py
+++ ship_o_cereal/cereal/views.py
@@ -12,12 +12,31 @@ def index(request):
 
     return render(request, 'cereal/index.html', context)
 
+def index(request):
+
+    if 'searchStr' not in request.GET and 'filterBy' not in request.GET:
+        return render(request, 'cereal/index.html')
+
+
+    else:
+        results = Product.objects
+        if 'searchStr' in request.GET:
+            searchParameter = request.GET['searchStr']
+            results = results.filter(name__icontains=searchParameter).order_by('name')
+        if 'filterBy' in request.GET:
+            filterParameter = request.GET['filterBy']
+            results = results.filter(manufacturer=filterParameter).order_by('name')
+        # context = {'products': Cereal.objects.filter(name__icontains=searchParameter).order_by('name')}
+        # brands = results.distinct('manufacturer').order_by('manufacturer')
+        # context = {'products': results, 'brands': brands}
+        context = {'products': results}
+        return render(request, 'cereal/index.html', context)
 # def cereal_by_id(request, id):
 #     return render(request, 'products/single_product.html', {
 #         'product': get_object_or_404(Cereal, pk=id)
 #     })
 
-def cereal_by_name(request, name):
+def product_by_name(request, name):
     return render(request, 'products/single_product.html', {
         'product': get_object_or_404(Product, name=name)
     })
