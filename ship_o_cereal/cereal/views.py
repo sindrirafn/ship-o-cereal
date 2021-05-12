5f7dd69b0609bfd9a5930eb89a62bcccfaf2e3dd
sindrirafn <sindrirafn12345@gmail.com>
2021-05-12 15:41:33 +0000

    smá commit svo allt sé up to date

--- ship_o_cereal/cereal/views.py
+++ ship_o_cereal/cereal/views.py
@@ -3,14 +3,14 @@ from cereal.models import Product
 # Create your views here.
 
 
-def index(request):
-    if 'orderBy' in request.GET:
-        orderParameter = request.GET['orderBy']
-        context = {'products': Product.objects.all().order_by(orderParameter)}
-    else:
-        context = {'products': Product.objects.all().order_by('name')}
-
-    return render(request, 'cereal/index.html', context)
+# def index(request):
+#     if 'orderBy' in request.GET:
+#         orderParameter = request.GET['orderBy']
+#         context = {'products': Product.objects.all().order_by(orderParameter)}
+#     else:
+#         context = {'products': Product.objects.all().order_by('name')}
+#
+#     return render(request, 'cereal/index.html', context)
 
 def index(request):
 
@@ -24,16 +24,14 @@ def index(request):
             results = results.filter(name__icontains=searchParameter).order_by('name')
         if 'filterBy' in request.GET:
             filterParameter = request.GET['filterBy']
-            results = results.filter(manufacturer=filterParameter).order_by('name')
+            results = results.filter(brand=filterParameter).order_by('name')
         # context = {'products': Cereal.objects.filter(name__icontains=searchParameter).order_by('name')}
         # brands = results.distinct('manufacturer').order_by('manufacturer')
         # context = {'products': results, 'brands': brands}
         context = {'products': results}
         return render(request, 'cereal/index.html', context)
-# def cereal_by_id(request, id):
-#     return render(request, 'products/single_product.html', {
-#         'product': get_object_or_404(Cereal, pk=id)
-#     })
+
+
 
 def product_by_name(request, name):
     return render(request, 'products/single_product.html', {
--- ship_o_cereal/front_page/views.py
+++ ship_o_cereal/front_page/views.py
@@ -6,24 +6,9 @@ from cereal.models import Product
 
 # Create your views here.
 def index(request):
-
-    if 'searchStr' not in request.GET and 'filterBy' not in request.GET:
-        return render(request, 'front_page/index.html')
+    return render(request, 'front_page/index.html')
 
 
-    else:
-        results = Product.objects
-        if 'searchStr' in request.GET:
-            searchParameter = request.GET['searchStr']
-            results = results.filter(name__icontains=searchParameter).order_by('name')
-        if 'filterBy' in request.GET:
-            filterParameter = request.GET['filterBy']
-            results = results.filter(manufacturer=filterParameter).order_by('name')
-        # context = {'products': Cereal.objects.filter(name__icontains=searchParameter).order_by('name')}
-        # brands = results.distinct('manufacturer').order_by('manufacturer')
-        # context = {'products': results, 'brands': brands}
-        context = {'products': results}
-        return render(request, 'products/search_results.html', context)
 
 
 
@@ -37,11 +22,3 @@ def register(request):
         'form': CustomUserCreationForm()
     })
 
-
-def product_by_name(request, name):
-    return render(request, 'products/single_product.html', {
-        'product': get_object_or_404(Product, name=name)
-    })
-
-# def filter_by_manufacturer(request, manufacturer):
-#     manufacturer = request.GET['brand']
