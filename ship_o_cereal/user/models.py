23867a378c464c08dea223f279be538956f20488
Steinar <steisaks@gmail.com>
2021-05-13 15:02:40 +0000

    user_profile tengt við register

--- ship_o_cereal/user/models.py
+++ ship_o_cereal/user/models.py
@@ -13,9 +13,14 @@ class SearchHistory(models.Model):
 
 class Profile(models.Model):
     user = models.OneToOneField(User, related_name='Profile', on_delete=models.CASCADE)
-    name = models.CharField(max_length=255, null = True)
-    email = models.CharField(max_length=255, null = True)
     profile_img = models.CharField(max_length=9999, default="https://img.icons8.com/material-rounded/200/000000/user.png")
+
+    def make_profile(sender, **kwargs):
+        user = kwargs["instance"]
+        if kwargs["created"]:
+            user_profile = Profile(user=user)
+            user_profile.save()
+    post_save.connect(make_profile, sender=User)
 
     def __str__(self):
         return str(self.id)
