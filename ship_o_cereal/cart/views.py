5c794a5fef4a9000f81fd0096f5672152db4c610
jonevert <jonp16@ru.is>
2021-05-14 17:29:52 +0000

    images in cart

--- ship_o_cereal/cart/views.py
+++ ship_o_cereal/cart/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
 from .models import *
+from cereal.models import CerealImage
 import json
 # Create your views here.
 
@@ -11,15 +12,21 @@ def index(request):
 """
 
 
+# GET gets all items in cart for logged in user and images
 def index(request):
     if request.user.is_authenticated:
         customer = request.user.Profile
         cart, created = Cart.objects.get_or_create(customer=customer, complete=False)
         items = cart.cartitem_set.all()
+        products = [item.product for item in items]
+        print(products)
+        images = CerealImage.objects.filter(cereal__in=products)
+        print(images)
     else:
         items = []
         cart = {'get_cart_total':0}
-    context = {'item': items, 'order':cart}
+        images = {'link': ''}
+    context = {'item': items, 'order':cart, 'images':images}
     return render(request, 'cart/index.html', context)
 
 
--- ship_o_cereal/cereal/models.py
+++ ship_o_cereal/cereal/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 
+# Model that keeps all the products
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=999, blank=True)
@@ -11,6 +12,8 @@ class Product(models.Model):
     price = models.FloatField()
 
 
+# Model that references products and keeps all images relating to products
+# as a string (link) to image
 class CerealImage(models.Model):
     image = models.CharField(max_length=9999)
     cereal = models.ForeignKey(Product, on_delete=models.CASCADE)
